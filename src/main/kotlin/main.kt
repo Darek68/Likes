48372232c022d87d9a763893eae8a34fb1bb6fff
Darek <darek68@list.ru>
2021-06-14 23:48:54 +0300

    Second commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,10 @@
 
 
 fun main() {
-
     print("Введите число лайков: ")
     val likes = readLine()!!.toInt()
-    val ending : String = if (likes % 10 == 1) "человеку" else "людям"
+
+    val ending : String = if (likes % 10 == 1 && likes % 100 != 11) "человеку" else "людям"
 
     println("Понравилось $likes $ending.")
-
 }
